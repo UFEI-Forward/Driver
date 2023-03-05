f48d821bf95464022279077ff5c6e46156b37daa
UFEI <wenhe.zhang94@gmail.com>
2023-03-05 00:13:26 -0500

    add test.c

---
+++ test.c
@@ -0,0 +1,52 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+//millisecond
+#define TIMEOUT 5000
+
+int times = 0;
+
+static struct timer_list chr_timer;
+
+//timer callback function which is called when timer expires
+void timer_callback(struct timer_list *timer)
+{
+	printk(KERN_INFO "Callling timer_callback() %i-th times", ++times);
+
+	//printk(KERN_ALERT "msecs_to_jiffies %i", msecs_to_jiffies(TIMEOUT));
+
+	// Re-enable the timer which will make this as periodic timer */
+	mod_timer(&chr_timer,msecs_to_jiffies(TIMEOUT));
+}
+ 
+static int __init chr_driver_init(void)
+{
+	printk(KERN_ERR "*********************************");
+	printk(KERN_INFO "chr_driver_init is running!");
+	
+	//setup your timer to call timer callback function */
+	timer_setup(&chr_timer,timer_callback,0);
+
+	//setup the timer interval to base on TIMEOUT Macro
+	//mod_timer(&chr_timer,jiffies + msecs_to_jiffies(TIMEOUT));
+
+	printk(KERN_ERR "jiffies is %i", jiffies);
+	printk(KERN_ERR "msecs_to_jiffies is %i", msecs_to_jiffies(TIMEOUT));
+	printk(KERN_ERR "jiffies + msecs_to_jiffies is %i", jiffies + msecs_to_jiffies(TIMEOUT));
+	
+	return 0;
+}
+ 
+void __exit chr_driver_exit(void)
+{
+	del_timer(&chr_timer);
+
+	printk(KERN_INFO "chr_driver_exit is running!");
+}
+ 
+module_init(chr_driver_init);
+module_exit(chr_driver_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("TechoGenius Academy");
+MODULE_DESCRIPTION("Character Device Driver using Interrupts");
